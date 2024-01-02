2b79473036e3cb3c3539905406ffae51f321f358
Willem Leuverink <willem@leuver.ink>
2024-01-02 14:48:47 +0100

    wip - working bundle manager

--- src/BundleManager.php
+++ src/BundleManager.php
@@ -2,14 +2,21 @@
 
 namespace Leuverink\Bundle;
 
+use Throwable;
 use SplFileInfo;
-use Leuverink\Bundle\Contracts\Bundler as BundlerContract;
-use Leuverink\Bundle\Contracts\BundleManager as BundleManagerContract;
+use Illuminate\Support\Facades\Storage;
 use Leuverink\Bundle\Traits\Constructable;
+use Leuverink\Bundle\Contracts\Bundler as BundlerContract;
+use Illuminate\Contracts\Filesystem\Filesystem as FilesystemContract;
+use Leuverink\Bundle\Contracts\BundleManager as BundleManagerContract;
+
 
 class BundleManager implements BundleManagerContract
 {
     use Constructable;
+
+    public const BUILD_DIR = 'bundle';
+    public const TEMP_DIR = 'bundle-tmp';
 
     protected readonly BundlerContract $bundler;
 
@@ -20,11 +27,52 @@ class BundleManager implements BundleManagerContract
 
     public function bundle(string $script): SplFileInfo
     {
-        $this->bundler->build(
-            inputPath: '',
-            outputPath: '',
-            fileName: ''
-        );
-        return new SplFileInfo('');
+        $fileName = "{$this->hash($script)}.min.js";
+
+        // TODO: Return cached file if available
+
+        // Create temporary input file
+        $this->tempDisk()->put($fileName, $script);
+
+        // Attempt bundling & cleanup
+        try {
+            $processed = $this->bundler->build(
+                inputPath: $this->tempDisk()->path(''),
+                outputPath: $this->buildDisk()->path(''),
+                fileName: $fileName
+            );
+        } catch (Throwable $e) {
+            $this->tempDisk()->delete($fileName);
+            // TODO: Consider raising a browser console error instead
+            throw $e;
+        } finally {
+            $this->tempDisk()->delete($fileName);
+        }
+
+        return $processed;
+    }
+
+    private function tempDisk(): FilesystemContract
+    {
+        return Storage::build([
+            'driver' => 'local',
+            'root' => storage_path('app/' . static::TEMP_DIR),
+        ]);
+    }
+
+    private function buildDisk(): FilesystemContract
+    {
+        return Storage::build([
+            'driver' => 'local',
+            'root' => storage_path('app/' . static::BUILD_DIR),
+        ]);
+    }
+
+    private function hash($input, $length = 8) {
+        // Create a SHA-256 hash of the input
+        $hash = hash('sha256', $input);
+
+        // Truncate the hash to the specified length
+        return substr($hash, 0, $length);
     }
 }
--- src/Bundlers/Bun.php
+++ src/Bundlers/Bun.php
@@ -3,9 +3,9 @@
 namespace Leuverink\Bundle\Bundlers;
 
 use Exception;
+use SplFileInfo;
 use Illuminate\Support\Facades\Process;
 use Leuverink\Bundle\Contracts\Bundler;
-use Symfony\Component\Finder\SplFileInfo;
 use Leuverink\Bundle\Traits\Constructable;
 use Leuverink\Bundle\Exceptions\BundlingFailedException;
 
@@ -16,12 +16,30 @@ class Bun implements Bundler
 
     public function build(string $inputPath, string $outputPath, string $fileName): SplFileInfo
     {
-        $output = Process::run(base_path('node_modules/.bin/bun build'))
+        $path = base_path('node_modules/.bin/');
+        $options = [
+            '--entrypoints' => $inputPath.$fileName,
+            '--outdir' => $outputPath,
+            '--minify'
+        ];
+
+        Process::run("{$path}bun build {$this->args($options)}")
             ->throw(function ($res): void {
                 throw new BundlingFailedException($res);
-            })->output();
+            });
 
-        return new SplFileInfo($output);
+        return new SplFileInfo($outputPath.$fileName);
+    }
+
+    private function args(array $options): string
+    {
+        return collect($options)->reduce(function($carry, $option, $key) {
+            return str($carry)
+                ->append($key == 0 ? '' : $key)->append(' ')
+                ->append($option)->append(' ')
+                ->replace('  ', ' ')
+                ->toString();
+        });
     }
 
 }

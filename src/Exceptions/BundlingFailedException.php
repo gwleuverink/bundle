927f85df1b1aac00f209089cdabc6cc1ee7baf40
Willem Leuverink <willem@leuver.ink>
2024-01-03 13:43:23 +0100

    add missing jsconfig solution

--- src/Exceptions/BundlingFailedException.php
+++ src/Exceptions/BundlingFailedException.php
@@ -31,6 +31,7 @@ class BundlingFailedException extends RuntimeException implements ProvidesSoluti
         return match (true) {
             str_contains($this->result->errorOutput(), 'bun: No such file or directory') => $this->bunNotInstalledSolution(),
             str_contains($this->result->errorOutput(), 'error: Could not resolve') => $this->moduleNotResolvableSolution(),
+            str_contains($this->result->errorOutput(), 'tsconfig.json: ENOENT') => $this->missingJsconfigFileSolution(),
             default => BaseSolution::create()
                 ->setSolutionTitle('Failed to run the following process:')
                 ->setSolutionDescription($this->result->command())
@@ -51,6 +52,13 @@ class BundlingFailedException extends RuntimeException implements ProvidesSoluti
         return BaseSolution::create()
                 ->setSolutionTitle("Unable to resolve module '{$module}'")
                 ->setSolutionDescription("{$module}. Try running `npm install {$module}` or check the path to the import if it's a script in your resources directory.");
+    }
+
+    private function missingJsconfigFileSolution()
+    {
+        return BaseSolution::create()
+                ->setSolutionTitle("jsconfig.json file missing")
+                ->setSolutionDescription('A jsconfig file is required in order to define your bundle\'s path mapping. Please create a jsconfig in your project root.');
     }
 
 }

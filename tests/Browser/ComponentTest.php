b75fe9bce92e31f1cc18cc98981a91089274f4b3
Willem Leuverink <willem@leuver.ink>
2024-01-13 10:26:43 +0100

    add tests for inline script variant

--- tests/Browser/ComponentTest.php
+++ tests/Browser/ComponentTest.php
@@ -59,11 +59,54 @@ class ComponentTest extends DuskTestCase
             JS, 1);
     }
 
+    /** @test */
     public function it_renders_script_inline_when_inline_prop_was_passed()
     {
+        $this->blade(<<< 'HTML'
+                <x-bundle import="~/alert" as="alert" inline />
+            HTML)
+            // Assert it doesn't render src attribute on the script tag
+            ->assertScript(<<< 'JS'
+                document.querySelectorAll('script[data-bundle="alert"')[0].hasAttribute('src')
+            JS, false)
+            // Assert script tag has no content
+            ->assertScript(<<< 'JS'
+                typeof document.querySelectorAll('script[data-bundle="alert"')[0].innerHTML
+            JS, 'string');
     }
 
     public function it_doesnt_render_script_inline_by_default()
     {
+        $this->blade(<<< 'HTML'
+                <x-bundle import="~/alert" as="alert" />
+            HTML)
+            // Assert it renders src attribute on the script tag
+            ->assertScript(<<< 'JS'
+                document.querySelectorAll('script[data-bundle="alert"')[0].hasAttribute('src')
+            JS, true)
+            // Assert script tag has no content
+            ->assertScript(<<< 'JS'
+                document.querySelectorAll('script[data-bundle="alert"')[0].innerHTML
+            JS, null);
+    }
+
+    public function it_throws_an_error_when_bundle_not_found_in_development()
+    {
+
+    }
+
+    public function it_doesnt_throw_an_error_when_bundle_not_found_in_production()
+    {
+
+    }
+
+    public function it_raises_console_error_when_bundle_not_found_in_production()
+    {
+
+    }
+
+    public function it_doesnt_raise_a_console_error_when_bundle_not_found_in_development()
+    {
+
     }
 }

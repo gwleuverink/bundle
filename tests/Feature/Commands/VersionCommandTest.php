c00956ce16130fdefec5be33d38c200e396d29d6
Willem Leuverink <willem@leuver.ink>
2024-05-29 12:57:34 +0200

    boyscouting

--- tests/Feature/Commands/VersionCommandTest.php
+++ tests/Feature/Commands/VersionCommandTest.php
@@ -1,13 +1,11 @@
 <?php
 
-use Illuminate\Console\Command;
-
 it('runs without crashing')
     ->artisan('bundle:version')
-    ->assertExitCode(Command::SUCCESS)
     ->expectsOutputToContain('Bundle')
     ->expectsOutputToContain('Laravel')
     ->expectsOutputToContain('PHP')
     ->expectsOutputToContain('Bun')
     ->expectsOutputToContain('LightningCSS')
-    ->expectsOutputToContain('Sass');
+    ->expectsOutputToContain('Sass')
+    ->assertSuccessful();

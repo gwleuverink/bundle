f499fb1e1a633bbf59e3d62ce4ae118b2c6a25b6
Willem Leuverink <willem@leuver.ink>
2024-05-31 23:02:23 +0200

    wip - tdd initable exports

---
+++ tests/Browser/InitableImportsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Leuverink\Bundle\Tests\Browser;
+
+use Leuverink\Bundle\Tests\DuskTestCase;
+
+// Pest & Workbench Dusk don't play nicely together
+// We need to fall back to PHPUnit syntax.
+
+class InitableImportsTest extends DuskTestCase
+{
+    /** @test */
+    public function it_invokes_imports_with_init_prop()
+    {
+        $this->blade(<<< 'HTML'
+                <x-import module="~/default-function" init />
+            HTML)
+            ->assertScript('window.test_evaluated', true);
+    }
+
+    /** @test */
+    public function it_doesnt_invoke_imports_without_init_prop()
+    {
+        $this->blade(<<< 'HTML'
+                <x-import module="~/default-function" />
+            HTML)
+            ->assertScript('window.test_evaluated', null);
+    }
+}
